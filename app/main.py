0ccaefdefa5c1d754a6ac75495a6ce05c03c2c18
Colin Campbell <prontobol@gmail.com>
2023-02-15 12:13:29 -0500

    Socket connection management

--- app/main.py
+++ app/main.py
@@ -1,7 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+import typing
 
 app = FastAPI()
 
 @app.get("/")
 def get_stuff() :
     return {"message":"hello World"}
+
+
+class ConnectionManager :
+    def __init__(self) -> None:
+        self.sockets: typing.Dict[str,list[WebSocket]] = {}
+    
+    async def add_connection(self,websocket:WebSocket, group_name:str)->WebSocket: 
+        await websocket.accept()
+        if group_name in self.sockets :
+            self.sockets[group_name].append(websocket)
+        else :
+            self.sockets[group_name] = []
+            self.sockets[group_name].append(websocket)
+
+    async def send_message(self,group_name:str, user_socket:WebSocket, message:str) :
+        if group_name in self.sockets :
+            listeners = self.sockets[group_name]
+            for listener in listeners :
+                if listener != user_socket :
+                    await listener.send_text(message)
+        else :
+            return 
+
+connectionsManager = ConnectionManager()
+
+@app.websocket("/group/{group_name}")
+async def group_socket(websocket:WebSocket,group_name:str,) :
+    await connectionsManager.add_connection(websocket,group_name)
+    while True :
+        data = await websocket.receive_text()
+        await connectionsManager.send_message(group_name,websocket,data)
