48ed313bbb77a3fee3f05ede97113782da8d46cb
Colin Campbell <prontobol@gmail.com>
2023-03-05 15:07:12 -0500

    Get group

--- app/routes/group_route.py
+++ app/routes/group_route.py
@@ -36,16 +36,28 @@ def create_group(group: GroupBase, db: Session = Depends(get_db), current_user:U
         db.commit()
         db.refresh(grp_admin)
 
-        print(db_group.confidants[0].user)
         grp_return = GroupReturn(name=db_group.name, id=db_group.id, created_by=db_group.created_by, confidants=get_confidants(group=db_group,db=db))
         return grp_return
     else :
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="This group already exists")
 
-@router.get("/groups", response_model=List[GroupReturn])
-def read_groups(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    groups = db.query(Group).offset(skip).limit(limit).all()
-    return groups
+@router.get("/", response_model=List[GroupReturn])
+def get_groups(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    groups : list[Group] = []
+
+    # first select where the user is a confidant
+    memberships : list[Confidant] = db.query(Confidant).filter(Confidant.user == current_user.id).all()
+    # second get the groups for which the confidant belongs to and build a list
+    for membership in memberships :
+        db_group : Group = db.query(Group).filter(Group.id == membership.group).first()
+        groups.append(db_group)
+
+    groups_rtn : GroupReturn = []
+    for db_group in groups:
+        group_rtn = GroupReturn(id=db_group.id, name=db_group.name, confidants=get_confidants(db_group,db), created_by=db_group.created_by)
+        groups_rtn.append(group_rtn)
+
+    return groups_rtn[skip:limit + skip]
 
 
 @router.get("/groups/{group_id}", response_model=GroupReturn)
